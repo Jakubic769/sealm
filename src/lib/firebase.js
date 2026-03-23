2b1b970f5fd5a7d89d37f38fefd4ed9934fb5f28
Z4XQ <jakubsuszek39@gmail.com>
2026-03-23 20:18:38 +0100

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,13 +4,13 @@ import { getAuth } from 'firebase/auth'
 import { getDatabase } from 'firebase/database'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD5_mSu6EuAbdQLsAB-coSgfYfknJqUKjA",
-  authDomain: "sealm-launcher.firebaseapp.com",
-  databaseURL: "https://sealm-launcher-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "sealm-launcher",
-  storageBucket: "sealm-launcher.firebasestorage.app",
-  messagingSenderId: "324612216576",
-  appId: "1:324612216576:web:cbd041583abc14fd1485e5"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 }
 
 const app = initializeApp(firebaseConfig)
